2f9999857e20c7451afaf54eca685392a943ea95
Jesse Taube <Mr.Bossman075@gmail.com>
2022-03-12 09:04:35 -0500

    clocks

--- drivers/clk/sunxi/clk_suniv.c
+++ drivers/clk/sunxi/clk_suniv.c
@@ -11,11 +11,65 @@
 #include <linux/bitops.h>
 
 static struct ccu_clk_gate suniv_gates[] = {
-	[CLK_BUS_SPI0]	= GATE(0x60, BIT(20)), // refer to arch/arm/include/asm/arch-sunxi/clock_sun6i.h
+	[CLK_USB_PHY0]		=  { 0x0cc, BIT(1) },
+
+	[CLK_BUS_DMA]		=  { 0x060, BIT(6) },
+	[CLK_BUS_MMC0]		=  { 0x060, BIT(8) },
+	[CLK_BUS_MMC1]		=  { 0x060, BIT(9) },
+	[CLK_BUS_DRAM]		=  { 0x060, BIT(14) },
+	[CLK_BUS_SPI0]		=  { 0x060, BIT(20) },
+	[CLK_BUS_SPI1]		=  { 0x060, BIT(21) },
+	[CLK_BUS_OTG]		=  { 0x060, BIT(24) },
+	[CLK_BUS_VE]		=  { 0x064, BIT(0) },
+	[CLK_BUS_LCD]		=  { 0x064, BIT(4) },
+	[CLK_BUS_DEINTERLACE]	=  { 0x064, BIT(5) },
+	[CLK_BUS_CSI]		=  { 0x064, BIT(8) },
+	[CLK_BUS_TVD]		=  { 0x064, BIT(9) },
+	[CLK_BUS_TVE]		=  { 0x064, BIT(10) },
+	[CLK_BUS_DE_BE]		=  { 0x064, BIT(12) },
+	[CLK_BUS_DE_FE]		=  { 0x064, BIT(14) },
+	[CLK_BUS_CODEC]		=  { 0x068, BIT(0) },
+	[CLK_BUS_SPDIF]		=  { 0x068, BIT(1) },
+	[CLK_BUS_IR]		=  { 0x068, BIT(2) },
+	[CLK_BUS_RSB]		=  { 0x068, BIT(3) },
+	[CLK_BUS_I2S0]		=  { 0x068, BIT(12) },
+	[CLK_BUS_I2C0]		=  { 0x068, BIT(16) },
+	[CLK_BUS_I2C1]		=  { 0x068, BIT(17) },
+	[CLK_BUS_I2C2]		=  { 0x068, BIT(18) },
+	[CLK_BUS_UART0]		=  { 0x068, BIT(20) },
+	[CLK_BUS_UART1]		=  { 0x068, BIT(21) },
+	[CLK_BUS_UART2]		=  { 0x068, BIT(22) },
 };
 
 static struct ccu_reset suniv_resets[] = {
-	[RST_BUS_SPI0]	= RESET(0x2c0, BIT(20)),
+	[RST_USB_PHY0]		=  { 0x0cc, BIT(0) },
+
+	[RST_BUS_DMA]		=  { 0x2c0, BIT(6) },
+	[RST_BUS_MMC0]		=  { 0x2c0, BIT(8) },
+	[RST_BUS_MMC1]		=  { 0x2c0, BIT(9) },
+	[RST_BUS_DRAM]		=  { 0x2c0, BIT(14) },
+	[RST_BUS_SPI0]		=  { 0x2c0, BIT(20) },
+	[RST_BUS_SPI1]		=  { 0x2c0, BIT(21) },
+	[RST_BUS_OTG]		=  { 0x2c0, BIT(24) },
+	[RST_BUS_VE]		=  { 0x2c4, BIT(0) },
+	[RST_BUS_LCD]		=  { 0x2c4, BIT(4) },
+	[RST_BUS_DEINTERLACE]	=  { 0x2c4, BIT(5) },
+	[RST_BUS_CSI]		=  { 0x2c4, BIT(8) },
+	[RST_BUS_TVD]		=  { 0x2c4, BIT(9) },
+	[RST_BUS_TVE]		=  { 0x2c4, BIT(10) },
+	[RST_BUS_DE_BE]		=  { 0x2c4, BIT(12) },
+	[RST_BUS_DE_FE]		=  { 0x2c4, BIT(14) },
+	[RST_BUS_CODEC]		=  { 0x2d0, BIT(0) },
+	[RST_BUS_SPDIF]		=  { 0x2d0, BIT(1) },
+	[RST_BUS_IR]		=  { 0x2d0, BIT(2) },
+	[RST_BUS_RSB]		=  { 0x2d0, BIT(3) },
+	[RST_BUS_I2S0]		=  { 0x2d0, BIT(12) },
+	[RST_BUS_I2C0]		=  { 0x2d0, BIT(16) },
+	[RST_BUS_I2C1]		=  { 0x2d0, BIT(17) },
+	[RST_BUS_I2C2]		=  { 0x2d0, BIT(18) },
+	[RST_BUS_UART0]		=  { 0x2d0, BIT(20) },
+	[RST_BUS_UART1]		=  { 0x2d0, BIT(21) },
+	[RST_BUS_UART2]		=  { 0x2d0, BIT(22) },
 };
 
 static const struct ccu_desc suniv_ccu_desc = {
--- include/dt-bindings/clock/suniv-ccu-f1c100s.h
+++ include/dt-bindings/clock/suniv-ccu-f1c100s.h
@@ -7,7 +7,24 @@
 #ifndef _DT_BINDINGS_CLK_SUNIV_F1C100S_H_
 #define _DT_BINDINGS_CLK_SUNIV_F1C100S_H_
 
+#define CLK_PLL_CPU		0
+#define CLK_PLL_AUDIO_BASE	1
+#define CLK_PLL_AUDIO		2
+#define CLK_PLL_AUDIO_2X	3
+#define CLK_PLL_AUDIO_4X	4
+#define CLK_PLL_AUDIO_8X	5
+#define CLK_PLL_VIDEO		6
+#define CLK_PLL_VIDEO_2X	7
+#define CLK_PLL_VE		8
+#define CLK_PLL_DDR0		9
+#define CLK_PLL_PERIPH		10
+
 #define CLK_CPU			11
+
+/* CPU clock is exported */
+
+#define CLK_AHB			12
+#define CLK_APB			13
 
 #define CLK_BUS_DMA		14
 #define CLK_BUS_MMC0		15
@@ -66,5 +83,9 @@
 #define CLK_VE			64
 #define CLK_CODEC		65
 #define CLK_AVS			66
+
+/* All bus gates, DRAM gates and mod clocks are exported */
+
+#define CLK_NUMBER		(CLK_AVS + 1)
 
 #endif
